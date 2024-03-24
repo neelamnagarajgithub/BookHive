b874b549b42f9dfe4553b366d7ff0d416136feff
Neelam Naga Raj <113004986+neelamnagarajgithub@users.noreply.github.com>
2024-03-24 17:49:29 +0530

    Books HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 const baseurl="https://bookhive-server.onrender.com/api/books";
 
 fetch(baseurl, { method:"GET" })
-  .then(response => response.json()) // Convert the response data to JSON
-  .then(data => console.log(data)) // Log the response data
-  .catch(error => console.error('Error:', error)); // Log any errors
+  .then(response => response.json()) 
+  .then(data => {
+    const html = data.map(book => `
+      <div>
+        <h2>${book.title}</h2>
+        <p>${book.description}</p>
+        <img src="book.coverImage" alt="Image">
+      </div>
+    `).join('');
+    document.body.innerHTML = html;
+  }) 
+  .catch(error => console.error('Error:', error)); 
