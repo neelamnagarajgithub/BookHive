6ca04db06abf244a59fc94ba633ea9cf0e1e58c4
Neelam Naga Raj <113004986+neelamnagarajgithub@users.noreply.github.com>
2024-03-24 21:10:49 +0530

    payment route

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,22 +12,33 @@ window.onhashchange = function () {
             <h2>${book.title}</h2>
             <p>${book.description}</p>
             <img src="${book.coverImage}" alt="Image">
-            <button onclick="window.location.hash='#/payment'">Buy</button>
+            <button onclick="buyBook('${JSON.stringify(book)}')">Buy</button>
           </div>
         `
           )
           .join("");
         document.body.innerHTML = html;
       })
-      .catch((error) => console.error("Error:", error));
-  }
-  if (window.location.hash === "#/payment") {
-    fetch(`${baseurl}/api/payment`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+
+
+//Handling the sending of book data to the backend for getting the payments
+      function buyBook(book) {
+        const bookData = JSON.parse(book);
+        window.location.hash = '#/payment';
+        // Now you can use bookData to send a POST request
+        fetch(`${baseurl}/api/create-checkout-session`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bookData),
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => console.error('Error:', error));
+      }
   }
   if (window.location.hash === "#/user/paymentsuccess") {
     const html = `<div>Payment Success</div>`;
